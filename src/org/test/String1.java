1a54e76a4a7dde2343b52207dd9ceff63791aa5e
Pr3ucando <pr3ucando@gmail.com>
2022-07-24 18:45:44 +0530

    new

---
+++ src/org/test/String1.java
@@ -0,0 +1,29 @@
+package org.test;
+
+public class String1 {
+
+	public static void main(String[] args) {
+		
+		String s1 = "Greens Technology";
+		int length = s1.length();
+		System.out.println(length);
+		
+		String s2 = "Seleniumtesting";
+		int length2 = s2.length();
+		System.out.println(length2);
+		
+		String s3 = "P r ee thi  P ar kk avi";
+		int length3 = s3.length();
+		System.out.println(length3);
+		
+		String s4 = "JAGADEESAN";
+		int length4 = s4.length();
+		System.out.println(length4);
+		
+		
+		
+				
+
+	}
+
+}
